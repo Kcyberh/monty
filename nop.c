7521264e2c095d2cbfb290944ba2712226aaaef7
kcyberh <kcyberh30@gmail.com>
2023-06-22 15:06:24 -0700

    monty

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- it is nothing
+  *@head: the stack head
+  *@counter: the line_number
+  *Return: it returns nothing
+  */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
